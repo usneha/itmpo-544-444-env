8b8fdfc9c6c35085df31b309f37cfe758deeb04e
usneha <usneha@hawk.iit.edu>
2015-10-04 16:58:29 -0500

    commiting the changes made to teh install webserver bash file

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update -y
+sudo apt-get install-y apache2 git
 
-sudo apt-get install-y apache2
+
+ git clone https://github.com/usneha/itmo-544-444-fall-2015.git 
+
+mv ./itmo-544-444-fall-2015/images/ /var/www/html/images
+
+mv ./itmo-544-444-fall-2015/index.html /var/www/html
 
 echo "Hello" > /tmp/hello.txt
