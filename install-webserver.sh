79cdd594298ca17aa8e4cce47700abe732e07e50
usneha <usneha@hawk.iit.edu>
2015-10-04 21:44:04 -0500

    Changed paths

--- install-webserver.sh
+++ install-webserver.sh
@@ -4,10 +4,10 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
 
-git clone https://github.com/usneha/itmo-544-444-env.git 
+git clone https://github.com/usneha/itmpo-544-444-env.git 
 
-mv ./itmo-544-444-env/page2.html  /var/www/html
+mv ./itmpo-544-444-env/page2.html  /var/www/html
 
-mv ./itmo-544-444-env/index.html /var/www/html
+mv ./itmpo-544-444-env/index.html /var/www/html
 
 echo "Hello" > /tmp/hello.txt
