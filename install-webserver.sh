33c403f27e488e745d746c5fd82e37eba953e6e6
usneha <usneha@hawk.iit.edu>
2015-10-04 19:24:46 -0500

    fixed typos in script and changed index file

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install-y apache2 git
+sudo apt-get install -y apache2 git
 
 
- git clone https://github.com/usneha/itmo-544-444-fall-2015.git 
+git clone https://github.com/usneha/itmo-544-444-fall-2015.git 
 
 mv ./itmo-544-444-fall-2015/images/ /var/www/html/images
 
