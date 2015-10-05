61fc9edc714d263cd264aad3cd818c27cdb05c32
usneha <usneha@hawk.iit.edu>
2015-10-04 19:59:14 -0500

    added scrpit

--- install-webserver.sh
+++ install-webserver.sh
@@ -4,10 +4,10 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
 
-git clone https://github.com/usneha/itmo-544-444-fall-2015.git 
+git clone https://github.com/usneha/itmo-544-444-env.git 
 
-mv ./itmo-544-444-fall-2015/images/ /var/www/html/images
+mv ./itmo-544-444-env/page2.html  /var/www/html
 
-mv ./itmo-544-444-fall-2015/index.html /var/www/html
+mv ./itmo-544-444-env/index.html /var/www/html
 
 echo "Hello" > /tmp/hello.txt
